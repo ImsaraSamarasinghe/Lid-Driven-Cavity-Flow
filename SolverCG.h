dbbb1887e47af7f78d2f62bc73115557bc7d737f
ImsaraSamarasinghe <imsara256@gmail.com>
2024-03-19 12:44:48 +0000

    Changes made during MPI

--- SolverCG.h
+++ SolverCG.h
@@ -6,7 +6,7 @@ public:
     SolverCG(int pNx, int pNy, double pdx, double pdy);
     ~SolverCG();
 
-    void Solve(double* b, double* x);
+    void Solve(double* b, double* x, int argc, char** argv);
 
 private:
     double dx;
@@ -18,9 +18,10 @@ private:
     double* z;
     double* t;
 
-    void ApplyOperator(double* p, double* t);
-    void Precondition(double* p, double* t);
-    void ImposeBC(double* p);
-
+    void ApplyOperator(double* in, double* out,double* top,double* bot,int& rank, int& size, int& local_rows);
+    void Precondition(double* in, double* out,int& rank,int& size,int& local_rows);
+    void ImposeBC(double* inout,int& rank,int& size,int& local_rows);
+    void Send_and_Receive(double* localMatrix,double* sendMatrixT,double* sendMatrixB, double* recvMatrixT, double* recvMatrixB,int& rank,int& size,int& local_rows);
+    void printSubArrayToFile(double* subArray, int rank,int k,int n,int local_rows, string name);
 };
 
